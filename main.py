3bceef126e814bad0bce7f93250c89bc7dc0426c
Maor <maorsa9@gmail.com>
2022-10-02 10:42:26 +0300

    added all HighScore with data to the game

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
-import tkinter as tk
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -29,18 +28,18 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
 
-    #Detect collision with tail.
+    # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
--- scoreboard.py
+++ scoreboard.py
@@ -2,21 +2,31 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.color("black")
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("Black")
         self.penup()
-        self.goto(10, 265)
+        self.goto(0, 268)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score:{self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score:{self.score} High Score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
         self.goto(0, 0)
         self.write("GAME OVER.", align=ALIGNMENT, font=FONT)
 
